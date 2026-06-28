9f501e592f5c46d1dd7d6f8df3d5a47ed6daf3c6
Tharunya <jayamnagatharunya@gmail.com>
2026-06-28 09:07:29 +0530

    Add factorial calculation for large numbers

---
+++ Arrays/factorials_of_large_numbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def factorial(self, n):
+        fact = 1
+        for i in range(2, n + 1):
+            fact *= i
+        return [int(digit) for digit in str(fact)]
+        

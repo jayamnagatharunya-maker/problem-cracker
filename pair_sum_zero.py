a3b33d30c3871b554c7e5fdfaa6e06df564bb981
Tharunya <jayamnagatharunya@gmail.com>
2026-06-28 08:38:47 +0530

    Delete pair_sum_zero.py

--- pair_sum_zero.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def getPairs(self, arr):
-        seen = set()
-        pairs = set()
-        for num in arr:
-            if -num in seen:
-                pairs.add(tuple(sorted((num, -num))))
-            seen.add(num)
-        return [list(pair) for pair in sorted(pairs)]

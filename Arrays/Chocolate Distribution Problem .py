16000ebe152386a5988032344c63f9befd6fd4a5
Tharunya <jayamnagatharunya@gmail.com>
2026-07-01 23:15:44 +0530

    Add solution for Chocolate Distribution Problem
    Implement the Chocolate Distribution Problem solution to find the minimum difference in chocolate distribution among students.

---
+++ Arrays/Chocolate Distribution Problem .py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMinDiff(self, arr, m):
+        n = len(arr)
+        if m == 0 or n == 0:
+            return 0
+        arr.sort()
+        ans = float('inf')
+        for i in range(n - m + 1):
+            ans = min(ans, arr[i + m - 1] - arr[i])
+        return ans for this i want to in github file name please sugget?
